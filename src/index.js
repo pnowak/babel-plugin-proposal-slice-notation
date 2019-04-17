7309e2570bfa2cf869aa9d32ba84e02adfaace1d
pnowak <piotr.nowak.25.02.1978@gmail.com>
2019-04-17 21:12:51 +0200

    has length property check

--- src/index.js
+++ src/index.js
@@ -5,4 +5,32 @@ import { types as t } from "@babel/core";
 export default declare(api => {
   api.assertVersion(7);
 
+  return {
+    name: "proposal-slice-notation",
+    inherits: syntaxSliceNotation,
+
+    function hasLengthProperty(object) {
+      if (typeof object === 'string') {
+        object = Object(object);
+      }
+
+      if ('length' in object) {
+        return true;
+
+      } else {
+        return false;
+      }
+    }
+
+    visitor: {
+      memberExpression(path) {
+        const { node, scope } = path;
+        const { object, property, computed, optional } = node;
+        if (computed !== true && !hasLengthProperty(object)) {
+          return;
+        }
+
+      },
+    },
+  };
 });
